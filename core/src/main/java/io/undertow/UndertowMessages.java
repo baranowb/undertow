a760f84c1d16c15bce03d81a23d35656bed8242c
baranowb <baranowb@gmail.com>
2021-07-16 13:25:37 +0200

    [UNDERTOW-1532] - switch to toRealPath rather than use absolutePath on init

--- core/src/main/java/io/undertow/UndertowMessages.java
+++ core/src/main/java/io/undertow/UndertowMessages.java
@@ -618,4 +618,7 @@ public interface UndertowMessages {
 
     @Message(id = 198, value = "Blocking write timed out after %s nanoseconds.")
     WriteTimeoutException blockingWriteTimedOut(long timeoutNanoseconds);
+
+    @Message(id = 199, value = "Failed to initialize path manager for '%s' path.")
+    RuntimeException failedToInitializePathManager(String path, @Cause IOException ioe);
 }
