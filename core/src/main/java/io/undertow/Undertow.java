c6f4bde8a403301f3aff9d75504a8bd0a07b2b0f
Flavia Rainone <frainone@redhat.com>
2026-02-17 18:44:40 -0300

    [UNDERTOW-1880] Revert "Merge pull request #1140 from Nordix/graceful-shutdown-of-http2"
    The fix caused failures in GracefulShutdownTestCase with proxy mode
    
    This reverts commit 0a6a3b2dfba61f14f0e7db36235793377ed2b5d6, reversing
    changes made to b403227f320c6e9a7a3d89b2154a2987aef72083.

--- core/src/main/java/io/undertow/Undertow.java
+++ core/src/main/java/io/undertow/Undertow.java
@@ -639,10 +639,6 @@ public final class Undertow {
             }
         }
 
-        public synchronized void stop() {
-            IoUtils.safeClose(channel);
-        }
-
         public synchronized void suspend() {
             suspended = true;
             channel.suspendAccepts();
